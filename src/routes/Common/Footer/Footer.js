48d4dd45e90607a064d31ea9f2b74efd8a7db044
ijTuhin <isratishu0209@gmail.com>
2022-05-23 01:40:54 +0600

    footer responsive mobile

--- src/routes/Common/Footer/Footer.js
+++ src/routes/Common/Footer/Footer.js
@@ -3,8 +3,8 @@ import { facebook, google1, youtube } from '../../../index';
 
 const Footer = () => {
     return (
-        <div className='py-10 bg-white border-t-2 grid grid-cols-5'>
-            <div className='md:flex md:justify-center md:items-start md:ml-10 col-span-1'>
+        <div className='py-10 bg-white border-t-2 md:grid md:grid-cols-5 flex flex-col-reverse'>
+            <div className='flex justify-center md:items-start items-center md:ml-10 md:col-span-1 py-5'>
                 <h2 className='font-medium'>Follow us on: </h2>
                 <div className='flex mx-2'>
                     <img className='w-7 h-7 mx-1 transition ease-in-out delay-50 duration-300 hover:translate-y-2 hover:rounded-full' src={facebook} alt="" />
@@ -13,8 +13,8 @@ const Footer = () => {
                     <img className='bg-white p-1 w-8 mx-1 transition ease-in-out delay-50 duration-300 hover:translate-y-2 hover:rounded-full' src={youtube} alt="" />
                 </div>
             </div>
-            <div className='md:flex md:justify-center md:items-start md:mx-56 col-span-4'>
-                <div className='mx-6'>
+            <div className='md:flex md:justify-center md:items-start md:mx-56 md:col-span-4'>
+                <div className='md:mx-6 md:mb-0 mb-6 mx-12'>
                     <h1 className='text-2xl text-sky-600 font-medium'>Chittagong Offiice/Factory</h1>
                     <p className='text-xl'>Employees</p>
                     <small>Khowaj Nagar, P.O.: Ajimpara, <br />
@@ -28,7 +28,7 @@ const Footer = () => {
                         Email : noor@goldensonbd.com
                         www.goldensonbd.com</small>
                 </div>
-                <div className='mx-6'>
+                <div className='md:mx-6 md:mb-0 mb-6 mx-12'>
                     <h1 className='text-2xl text-sky-600 font-medium'>Share Office/Dhaka Office</h1>
                     <p className='text-xl'>Factory Space</p>
                     <small>Road # 90, House # 10/B, 1st Floor <br />
@@ -44,7 +44,7 @@ const Footer = () => {
                         (EXT: 203), Fax: +88 02 55052100, <br />
                         Email : noor@goldensonbd.com</small>
                 </div>
-                <div className='mx-6'>
+                <div className='md:mx-6 md:mb-0 mb-6 mx-12'>
                     <h1 className='text-2xl text-sky-600 font-medium'>Registered Office</h1>
                     <p className='text-xl'>Retail Space</p>
                     <small>Khowaj Nagar, P.O.: Ajimpara <br />
