6b14accb2677e61c1d92de4347a382a3d048b798
ijTuhin <isratishu0209@gmail.com>
2022-05-22 13:51:38 +0600

    added animation on banner heading text

--- src/routes/HomePage/Banner/Banner.js
+++ src/routes/HomePage/Banner/Banner.js
@@ -3,6 +3,16 @@ import './Banner.css';
 const Banner = () => {
     return (
         <div className='banner'>
+            <div class="banner-heading">
+                <div class="banner-heading-inner w-full p-3 bg-slate-300/60 my-20 border-l-4 border-blue-800">
+                    <div class="first-txt">
+                        <div class="first-txt-inner text-4xl text-sky-600">Keyframé</div>
+                    </div>
+                    <div class="second-txt">
+                        <div class="second-txt-inner text-6xl text-sky-700">Artistes</div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
