b89f3f20426ce75208cc4974cd4e99fc59d5c91f
ijTuhin <isratishu0209@gmail.com>
2022-05-23 19:55:45 +0600

    blog page done

--- src/routes/Blogs/Blogs.js
+++ src/routes/Blogs/Blogs.js
@@ -5,7 +5,7 @@ const Blogs = () => {
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mx-10 my-16 md:mx-40">
 
             <div id="multiCollapseExample1">
-                <div className="block p-6 rounded-lg shadow-lg bg-white">
+                <div className="p-6 rounded-lg shadow-lg bg-white">
                     <span className='text-gray-400'>### Answer No. 1</span> <br />
                     <span className='font-medium text-sky-700 text-lg'>The performance of a React Application will be improved by,</span><br />
                     * Keeping component state local where necessary. <br />
@@ -17,7 +17,7 @@ const Blogs = () => {
             </div>
 
             <div id="multiCollapseExample2">
-                <div className="block p-6 rounded-lg shadow-lg bg-white">
+                <div className="p-6 rounded-lg shadow-lg bg-white">
                     <span className='text-gray-400'>### Answer No. 2</span> <br />
                     <span className='font-medium text-sky-700 text-lg'>There are four main types of state that we need to properly manage in our React apps,</span><br />
                     - Local state, <br />
@@ -28,7 +28,7 @@ const Blogs = () => {
             </div>
 
             <div id="multiCollapseExample1">
-                <div className="block p-6 rounded-lg shadow-lg bg-white">
+                <div className="p-6 rounded-lg shadow-lg bg-white">
                     <span className='text-gray-400'>### Answer No. 3</span> <br />
                     <span className='font-medium text-sky-700 text-lg'>How Prototypal Inheritance works</span><br />
                     It is a method by which an object can inherit the properties and methods of another object. to get and set the Prototype of an object, we use Object. Each object has a private property which holds a link to another object called its prototype. That prototype object has a prototype of its own, and so on until an object is reached with null as its prototype.
@@ -36,19 +36,21 @@ const Blogs = () => {
             </div>
 
             <div id="multiCollapseExample2">
-                <div className="block p-6 rounded-lg shadow-lg bg-white">
-                    <span className='text-gray-400'>### Answer No. 4</span> <br />
-                    <span className='font-medium text-sky-700 text-lg'>Suppose, I have an array of products. Each product has a name, price, description, etc. How will I implement a search to find products by name?</span><br />
-                    As we need to fetch value from a object which is in an array. So, I'll go with, <br /> products[x].products.name
+                <div className="p-6 rounded-lg shadow-lg bg-white">
+                    <div className='mb-4 pb-0.5'>
+                        <span className='text-gray-400'>### Answer No. 4</span> <br />
+                        <span className='font-medium text-sky-700 text-lg'>There is an array of products. Each product has a name, price, description, etc. Implementing a search to find products by name</span><br />
+                        As we need to fetch value from a object which is in an array. So, first we need to map each element which is an object from the array, <br /> <span className='text-sky-600 mx-auto text-center'>products[x].products.name</span>
+                    </div>
                 </div>
+
             </div>
 
             <div id="multiCollapseExample2">
-                <div className="block p-6 rounded-lg shadow-lg bg-white">
+                <div className="p-6 rounded-lg shadow-lg bg-white">
                     <span className='text-gray-400'>### Answer No. 5</span> <br />
-                    <span className='font-medium text-sky-700 text-lg'>The purpose of jwt & How it works</span><br />
-                    to share security information between two parties — a client and a server. JWT is a token based stateless authentication mechanism. <br />
-                    It works in this way that, Server generates a token that certifies user's identity, and sends it to the client. The client will send the token back to the server for each subsequent request, so the server knows the request comes from a particular identity.
+                    <span className='font-medium text-sky-700 text-lg'>What is a unit test? Why should write unit tests?</span><br />
+                    UNIT TESTING is a type of software testing where individual units or components of a software are tested. The purpose is to validate that each unit of the software code performs as expected. It allows software developers to actually think through the design of the software and what has to be done before they write the code
                 </div>
             </div>
         </div>
