b546ed6d2cf6af04a3d96ca5b1f719fa2cc2eb87
ijTuhin <isratishu0209@gmail.com>
2022-05-23 18:23:20 +0600

    contact section & about section mobile view responsive

--- src/routes/HomePage/AboutUs/AboutUs.js
+++ src/routes/HomePage/AboutUs/AboutUs.js
@@ -6,17 +6,17 @@ import { Link } from 'react-router-dom';
 
 const AboutUs = () => {
     return (
-        <div className='relative flex'>
-            <div className='bg-sky-800 w-[35%] h-[500px]'>
+        <div className='relative flex md:flex-row flex-col-reverse'>
+            <div className='bg-sky-800 md:w-[35%] h-[500px]'>
                 <ScrollAnimation animateIn="animate__backInLeft animate__slower">
-                    <img className='md:w-96 w-44 relative left-[35%] top-10' src={banner1} alt='' />
+                    <img className='md:w-96 w-72 relative md:left-[35%] left-[10%] md:top-10 top-24' src={banner1} alt='' />
                 </ScrollAnimation>
                 <ScrollAnimation animateIn="animate__backInRight animate__slow">
-                    <img className='md:w-96 w-44 relative left-[70%] right-10 bottom-14' src={banner1} alt='' />
+                    <img className='md:w-96 w-72 relative md:left-[70%] left-[40%] md:bottom-14 -bottom-8' src={banner1} alt='' />
                 </ScrollAnimation>
             </div>
-            <div className='w-[35%] ml-auto mr-[10%] my-auto'>
-                <h2 className='text-5xl font-serif text-sky-700 font-medium'>About Us</h2>
+            <div className='md:w-[35%] md:ml-auto md:mr-[10%] md:my-auto m-[10%] md:text-base text-sm'>
+                <h2 className='md:text-5xl text-4xl font-serif text-sky-700 font-medium'>About Us</h2>
                 <hr className='w-[75%] bg-gray-500 my-1' />
                 <p>ASUS is passionate about technology and driven by innovation. We dream, we dare and we strive to create an effortless and joyful digital life for everyone. We're always in search of incredible ideas and experiences, and we aspire to deliver the incredible in everything we do. ASUSTeK COMPUTER INC. and its affiliated entities companies use cookies and similar technologies to perform essential online functions, such as authentication and security. You may disable these by changing your cookies setting through browser, but this may affect how this website functions.
                     Also, ASUS uses some analytics, targeting, adverting and video-embedded cookies provided by ASUS or third parties. Please click a button here to choose your preference for these types of cookies. Please note that you can configure cookies settings by accessing the browser you install at any time. <Link to='/about-us' className='text-sky-600 hover:text-sky-700 flex items-center'><span>read more</span> <img src={rightArrow} className="arrow w-3.5 mt-[2.5px] mx-2" alt="" /></Link> </p>
--- src/routes/HomePage/BusinessSummary/BusinessSummary.js
+++ src/routes/HomePage/BusinessSummary/BusinessSummary.js
@@ -5,7 +5,7 @@ const BusinessSummary = () => {
         <div className='py-10 bg-sky-100'>
             <h2 className='text-3xl text-center md:block hidden'>Our Business Summary in Numbers</h2>
             <h2 className='text-3xl text-center md:hidden block'>Our Business <br />Summary in Numbers</h2>
-            <div className='md:flex md:justify-center md:items-center md:mx-56 grid grid-cols-2 gap-y-5 gap-x-3 mx-6 my-12'>
+            <div className='md:flex md:justify-center md:items-center md:mx-56 grid grid-cols-2 gap-y-5 gap-x-3 mx-[10%] my-12'>
                 <div className='flex flex-col justify-center items-center px-2'>
                     <img src={workers} className="w-16" alt=''/>
                     <h1 className='text-4xl text-sky-600 font-medium'>5,000+</h1>
@@ -13,13 +13,13 @@ const BusinessSummary = () => {
                     <small className='text-center'>Our strong team of over 5000+ employees are working relentlessly in almost all 64 districts of the country with a mission to generate substantial stakeholder value.</small>
                 </div>
                 <div className='flex flex-col justify-center items-center px-2'>
-                    <img src={factory} className="w-14" alt=''/>
+                    <img src={factory} className="w-16" alt=''/>
                     <h1 className='text-4xl text-sky-600 font-medium'>1M</h1>
                     <p className='text-xl'>Factory Space</p>
                     <small className='text-center'>At over 20 factories and industrial zones in Bangladesh, Zaman Group proudly owns over 1,000,000 square feet of factory space in various parts of the country.</small>
                 </div>
                 <div className='flex flex-col justify-center items-center px-2'>
-                    <img src={retails} className="w-12" alt=''/>
+                    <img src={retails} className="w-14" alt=''/>
                     <h1 className='text-4xl text-sky-600 font-medium'>0.3M</h1>
                     <p className='text-xl'>Retail Space</p>
                     <small className='text-center'>In order the become to the largest global brand electronics retailer in the country, Zaman Group is targeting exponential growth in terms of retail space acquisition.</small>
