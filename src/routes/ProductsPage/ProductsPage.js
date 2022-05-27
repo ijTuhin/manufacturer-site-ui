0b533470d434a64e48c6e1d2105c09f54eee71ee
ijTuhin <isratishu0209@gmail.com>
2022-05-28 02:08:11 +0600

    products data fetch from Db to UI

--- src/routes/ProductsPage/ProductsPage.js
+++ src/routes/ProductsPage/ProductsPage.js
@@ -6,7 +6,7 @@ const ProductsPage = () => {
     const [products, setProducts] = useState([]);
 
     useEffect(() => {
-        fetch('products.json')
+        fetch('http://localhost:5000/product')
             .then(res => res.json())
             .then(data => {
                 setProducts(data);
