321dfc6782b5fbd3c2530302ef310a25e2fa949d
ijTuhin <isratishu0209@gmail.com>
2022-05-28 22:01:21 +0600

    my-orders & purchase structure done

--- src/routes/ForUser/MyOrderPage/MyOrderPage.js
+++ src/routes/ForUser/MyOrderPage/MyOrderPage.js
@@ -2,8 +2,48 @@ import React from 'react';
 
 const MyOrderPage = () => {
     return (
-        <div className='my-56'>
-            <h1>My Order</h1>
+        <div className='mx-20 my-10 h-screen'>
+            <div class="flex flex-col">
+                <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
+                    <div class="py-2 inline-block min-w-full sm:px-6 lg:px-8">
+                        <div class="overflow-hidden">
+                            <table class="min-w-full">
+                                <thead class="bg-white border-b">
+                                    <tr>
+                                        <th scope="col" class="text-sm font-medium w-20 text-gray-900 px-6 py-4">
+                                            UserId
+                                        </th>
+                                        <th scope="col" class="text-sm font-medium w-96 text-center text-gray-900 px-6 py-4">
+                                            Product
+                                        </th>
+                                        <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4">
+                                            Quantity
+                                        </th>
+                                        <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4">
+                                            Payment
+                                        </th>
+                                        
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr class="bg-white border-b transition duration-300 ease-in-out hover:bg-gray-100">
+                                        <td class="px-6 py-4 whitespace-nowrap text-sm text-center font-medium text-gray-900">1</td>
+                                        <td class="text-sm text-gray-900 text-center font-light px-6 py-4 whitespace-nowrap">
+                                            Mark
+                                        </td>
+                                        <td class="text-sm text-gray-900 text-center font-light px-6 py-4 whitespace-nowrap">
+                                            Otto
+                                        </td>
+                                        <td class="text-sm text-gray-900 text-center font-light px-6 py-4 whitespace-nowrap">
+                                            @mdo
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
--- src/routes/ForUser/PurchasePage/PurchasePage.js
+++ src/routes/ForUser/PurchasePage/PurchasePage.js
@@ -1,9 +1,27 @@
+import { faMinus, faPlus } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 
 const PurchasePage = () => {
     return (
-        <div className='my-56'>
-            <h1>Purchase</h1>
+        <div className='h-screen flex flex-col items-center'>
+            <div className='w-96 p-3 border-b border-gray-700'>
+                <div class="px-2 w-full flex justify-center">
+                    <img src="" alt="" />
+                </div>
+                <h1 className='text-2xl font-medium'>Name</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum corporis suscipit facilis qui expedita amet.</p>
+                <p>Price: $</p>
+                <p>Available: </p>
+                <p>Minimum: </p>
+            </div>
+            <div className='flex items-center justify-between w-96'>
+                <p className='px-3'>Order: 5</p>
+                <div>
+                    <button className='border bg-white hover:bg-gray-100 rounded-sm text-red-600 px-3 py-0.5'><FontAwesomeIcon icon={faPlus}></FontAwesomeIcon></button>
+                    <button className='border bg-white hover:bg-gray-100 rounded-sm text-red-600 px-3 py-0.5'><FontAwesomeIcon icon={faMinus}></FontAwesomeIcon></button>
+                </div>
+            </div>
         </div>
     );
 };
