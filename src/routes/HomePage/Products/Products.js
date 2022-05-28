40d0d85a71704df505c96d93a93e579452853f52
ijTuhin <isratishu0209@gmail.com>
2022-05-28 16:19:19 +0600

    deployed to heroku

--- src/routes/HomePage/Products/Products.js
+++ src/routes/HomePage/Products/Products.js
@@ -7,7 +7,7 @@ const Products = () => {
     const [products, setProducts] = useState([]);
 
     useEffect(() => {
-        fetch('http://localhost:5000/product')
+        fetch('https://dry-journey-38445.herokuapp.com//product')
             .then(res => res.json())
             .then(data => {
                 setProducts(data);
