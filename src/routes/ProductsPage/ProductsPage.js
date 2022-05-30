ef253ac84898f56549f0b16f173ec4884b63f206
ijTuhin <isratishu0209@gmail.com>
2022-05-30 17:17:12 +0600

    product page responsive

--- src/routes/ProductsPage/ProductsPage.js
+++ src/routes/ProductsPage/ProductsPage.js
@@ -19,7 +19,7 @@ const ProductsPage = () => {
                 <div className='w-[68%] border-b border-gray-400 mt-16'>
                     <h1 className='text-2xl px-2'>All Products</h1>
                 </div>
-                <div className='grid grid-cols-3 gap-y-5 gap-x-16'>
+                <div className='grid md:grid-cols-3 grid-cols-1 gap-y-5 gap-x-16'>
                     {
                         products.map(product => <Product
                             key={product._id}
