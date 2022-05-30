745c02f3c2ef8c947212509fc418a205c4fb1158
ijTuhin <isratishu0209@gmail.com>
2022-05-30 19:52:22 +0600

    add products mobile responsive

--- src/routes/ForAdmin/AddProduct/AddProduct.js
+++ src/routes/ForAdmin/AddProduct/AddProduct.js
@@ -24,11 +24,12 @@ const AddProduct = () => {
 
 
     return (
-        <div>
-            <div className="md:w-96 w-72 my-10 py-6 px-10 rounded-lg shadow-xl bg-white mx-auto">
-                <h1 className='text-center text-xl mb-4'>Add New Items</h1>
-                <form onSubmit={handleSubmit(onSubmit)}>
-                    <input className="form-control mb-2 block
+        <div className='h-screen'>
+            <div className="md:w-96 w-72 md:my-10 py-6 md:px-10 px-6 rounded-lg shadow-xl bg-white mx-auto">
+                <div>
+                    <h1 className='text-center text-xl mb-4'>Add New Items</h1>
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                        <input className="form-control mb-2 block
         w-full
         px-3
         md:py-1.5 py-1
@@ -43,7 +44,7 @@ const AddProduct = () => {
         m-0
         focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" value={user.email} placeholder="Inventory Item Name" {...register("email")} />
 
-                    <input className="form-control mb-2 block
+                        <input className="form-control mb-2 block
         w-full
         px-3
         md:py-1.5 py-1
@@ -58,7 +59,7 @@ const AddProduct = () => {
         m-0
         focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" placeholder="Inventory Item Name" {...register("name", { required: true, maxLength: 20 })} />
 
-                    <input className="form-control mb-2 block
+                        <input className="form-control mb-2 block
         w-full
         px-3
         md:py-1.5 py-1
@@ -73,7 +74,7 @@ const AddProduct = () => {
         m-0
         focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"  type="number" placeholder="Price" {...register("price", { required: true })} />
 
-                    <input className="form-control mb-2 block
+                        <input className="form-control mb-2 block
         w-full
         px-3
         md:py-1.5 py-1
@@ -88,7 +89,7 @@ const AddProduct = () => {
         m-0
         focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"  type="number" placeholder="Total stored item" {...register("available", { required: true })} />
 
-                    <input className="form-control mb-2 block
+                        <input className="form-control mb-2 block
         w-full
         px-3
         md:py-1.5 py-1
@@ -103,7 +104,7 @@ const AddProduct = () => {
         m-0
         focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"  type="number" placeholder="Minimum order quantity" {...register("minimum", { required: true })} />
 
-                    <input className="form-control mb-2 block
+                        <input className="form-control mb-2 block
         w-full
         px-3
         md:py-1.5 py-1
@@ -119,7 +120,7 @@ const AddProduct = () => {
         focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" placeholder='Photo URL' type="text" {...register("img")} />
 
 
-                    <textarea className="
+                        <textarea className="
         form-control mb-2
         block
         w-full
@@ -136,8 +137,9 @@ const AddProduct = () => {
         m-0
         focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none
       " placeholder='Description' {...register("description")} />
-                    <input type="submit" value="Add Service" className='border border-slate-300 bg-slate-200 text-slate-700 rounded w-full px-2.5 py-0.5 my-1 hover:bg-slate-300 hover:border-slate-300 hover:text-slate-800' />
-                </form>
+                        <input type="submit" value="Add Service" className='border border-slate-300 bg-slate-200 text-slate-700 rounded w-full px-2.5 py-0.5 my-1 hover:bg-slate-300 hover:border-slate-300 hover:text-slate-800' />
+                    </form>
+                </div>
             </div>
         </div>
     );
